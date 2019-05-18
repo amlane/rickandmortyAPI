a0791cd322706620fa28c18f57bcfdc9ed96ca6c
BrannanC <brannanconrad@gmail.com>
2019-05-18 16:30:45 -0500

    Changed CDU to fetchData() and called it in increment/decrement, added disabled to buttons

--- src/App.js
+++ src/App.js
@@ -1,91 +1,111 @@
-import React from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import React from "react";
+import { Route, NavLink } from "react-router-dom";
 
-import './styling/App.css';
+import "./styling/App.css";
 
-import axios from 'axios';
-import CharacterList from './components/CharacterList';
-import Home from './components/Home';
-import Character from './components/Character';
-import About from './components/About';
+import axios from "axios";
+import CharacterList from "./components/CharacterList";
+import Home from "./components/Home";
+import Character from "./components/Character";
+import About from "./components/About";
 
-
-class App extends React.Component{
-  constructor(){
+class App extends React.Component {
+  constructor() {
     super();
     this.state = {
       data: [],
-      pageNum: 1
-    }
+      pageNum: 1,
+      totalPages: 0
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     axios
-    .get(`https://rickandmortyapi.com/api/character/?page=${this.state.pageNum}`)
-    .then( res => {
-      this.setState({ data: res.data.results, pageNum: this.state.pageNum  })
-    })
-    .catch( err => {
-      console.log(err)
-    })
+      .get(
+        `https://rickandmortyapi.com/api/character/?page=${this.state.pageNum}`
+      )
+      .then(res => {
+        console.log(res.data.info.pages);
+        this.setState({
+          data: res.data.results,
+          totalPages: res.data.info.pages
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
-  componentDidUpdate(){
+  fetchData() {
+    console.log(this.state.pageNum);
     axios
-    .get(`https://rickandmortyapi.com/api/character/?page=${this.state.pageNum}`)
-    .then( res => {
-      this.setState({ data: res.data.results })
-    })
-    .catch( err => {
-      console.log(err)
-    })
+      .get(
+        `https://rickandmortyapi.com/api/character/?page=${this.state.pageNum}`
+      )
+      .then(res => {
+        this.setState({ data: res.data.results });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   pageChangeDecrement = e => {
     e.preventDefault();
-    if(this.state.pageNum === 1) return;
-    this.setState({ 
-      pageNum: --this.state.pageNum
-     })
-  }
+    if (this.state.pageNum === 1) return;
+    this.setState(
+      prev => ({
+        pageNum: --prev.pageNum
+      }),
+      this.fetchData()
+    );
+  };
 
   pageChangeIncrement = e => {
     e.preventDefault();
-    this.setState({ 
-      pageNum: ++this.state.pageNum
-     })
-  }
+    if (this.state.pageNum === this.state.totalPages) return;
+    this.setState(
+      prev => ({
+        pageNum: ++prev.pageNum
+      }),
+      this.fetchData()
+    );
+  };
 
-
-  render(){
+  render() {
     return (
       <div className="App">
-
         <nav className="nav-links">
-          <NavLink exact to="/">Home</NavLink>{" "}
-          <NavLink exact to="/character-list">Characters</NavLink>
-          <NavLink exact to="/about">About</NavLink>
+          <NavLink exact to="/">
+            Home
+          </NavLink>{" "}
+          <NavLink exact to="/character-list">
+            Characters
+          </NavLink>
+          <NavLink exact to="/about">
+            About
+          </NavLink>
         </nav>
 
         <Route exact path="/" component={Home} />
-        <Route 
-        exact path="/character-list" 
-        render={ props => 
-        <CharacterList 
-        {...props} 
-        data={this.state.data} 
-        pageNum={this.state.pagNum}
-        pageChangeIncrement={this.pageChangeIncrement}
-        pageChangeDecrement={this.pageChangeDecrement}
-        /> } />
-        <Route 
-        path="/character-list/:id"
-        render={ props => 
-        <Character 
-        {...props}
-        data={this.state.data} 
-        /> }
-        /> 
+        <Route
+          exact
+          path="/character-list"
+          render={props => (
+            <CharacterList
+              {...props}
+              data={this.state.data}
+              pageNum={this.state.pageNum}
+              totalPages={this.state.totalPages}
+              pageChangeIncrement={this.pageChangeIncrement}
+              pageChangeDecrement={this.pageChangeDecrement}
+            />
+          )}
+        />
+        <Route
+          path="/character-list/:id"
+          render={props => <Character {...props} data={this.state.data} />}
+        />
         <Route path="/about" component={About} />
       </div>
     );
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-function CharacterList(props){
-
-    return (
-        <>
-        <div className="btn-wrapper">
-        <button onClick={props.pageChangeDecrement}>Prev</button> <button onClick={props.pageChangeIncrement}>Next</button>
-        </div>
-        <div className="character-list">
-        {props.data.map( item => {
-            return (
-            <Link key={item.id} className="card-links" to={`/character-list/${item.id}`}>
-            <div className="character-card">
-                <img className="character-avatar" src={item.image} alt={item.name} />
-             </div>
-             </Link>
-            )
+function CharacterList(props) {
+  return (
+    <>
+      <div className="btn-wrapper">
+        <button
+          disabled={props.pageNum === 1}
+          onClick={props.pageChangeDecrement}
+        >
+          Prev
+        </button>
+        <button
+          disabled={props.pageNum === props.totalPages}
+          onClick={props.pageChangeIncrement}
+        >
+          Next
+        </button>
+      </div>
+      <div className="character-list">
+        {props.data.map(item => {
+          return (
+            <Link
+              key={item.id}
+              className="card-links"
+              to={`/character-list/${item.id}`}
+            >
+              <div className="character-card">
+                <img
+                  className="character-avatar"
+                  src={item.image}
+                  alt={item.name}
+                />
+              </div>
+            </Link>
+          );
         })}
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default CharacterList;
